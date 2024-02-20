0d474dfdb363e2b8df00bef3577ee60345112aff
Marchi8 <luandeoliveirasoares@gmail.com>
2024-02-20 18:01:23 -0300

    Trying to solve dependency errors

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,8 +4,84 @@ import { WebView } from "react-native-webview";
 import Constants from "expo-constants";
 import { requestTrackingPermissionsAsync } from "expo-tracking-transparency";
 import { Platform } from "react-native";
+import * as Notifications from "expo-notifications";
+import * as Device from "expo-device";
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldPlaySound: true,
+    shouldSetBadge: true,
+    shouldShowAlert: true,
+  }),
+});
+
+Notifications.requestPermissionsAsync().then((status) => {
+  return;
+});
 
 export default function Home({ navigation }) {
+  const notificationListener = useRef();
+  const responseListener = useRef();
+
+  const handleCallNotification = async () => {
+    let token;
+
+    if (Device.isDevice) {
+      const { status: existingStatus } =
+        await Notifications.getPermissionsAsync();
+      let finalStatus = existingStatus;
+      if (existingStatus !== "granted") {
+        const { status } = await Notifications.requestPermissionsAsync();
+        finalStatus = status;
+      }
+      if (finalStatus !== "granted") {
+        return;
+      }
+      token = (await Notifications.getExpoPushTokenAsync()).data;
+    } else {
+      return;
+    }
+
+    return token;
+  };
+
+  React.useEffect(() => {
+    handleCallNotification().then((token) => setExpoPushToken(token));
+
+    notificationListener.current =
+      Notifications.addNotificationReceivedListener((notification) => {
+        setNotification(notification);
+      });
+
+    responseListener.current =
+      Notifications.addNotificationResponseReceivedListener((response) => {
+        console.log(response);
+      });
+
+    messaging()
+      .getInitialNotification()
+      .then((remoteMessage) => {
+        if (remoteMessage) {
+          return;
+        }
+      });
+
+    messaging().onNotificationOpenedApp(async (remoteMessage) => {
+      return;
+    });
+
+    messaging().setBackgroundMessageHandler(async (remoteMessage) => {
+      return;
+    });
+
+    return () => {
+      Notifications.removeNotificationSubscription(
+        notificationListener.current
+      );
+      Notifications.removeNotificationSubscription(responseListener.current);
+    };
+  }, []);
+
   const handleBackPress = () => {
     if (navigation.isFocused()) {
       BackHandler.exitApp();
